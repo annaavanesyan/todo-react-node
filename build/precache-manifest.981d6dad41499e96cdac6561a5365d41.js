8da8787919cd2c80212d52fcbfffa550b841a5c9
annaavanesyan <anna.avanesyan1998@gmail.com>
2019-09-01 23:06:37 +0400

    added client build with babel compiled files

---
+++ build/precache-manifest.981d6dad41499e96cdac6561a5365d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6fc4ae62c22887984312e1bb0fbd62",
+    "url": "/annaavanesyan/todo-react-node/index.html"
+  },
+  {
+    "revision": "e83941c7cdd3d3ee3e9d",
+    "url": "/annaavanesyan/todo-react-node/static/css/main.666bf3b4.chunk.css"
+  },
+  {
+    "revision": "4e98a607afecefd173de",
+    "url": "/annaavanesyan/todo-react-node/static/js/2.8488a448.chunk.js"
+  },
+  {
+    "revision": "e83941c7cdd3d3ee3e9d",
+    "url": "/annaavanesyan/todo-react-node/static/js/main.5532e3b2.chunk.js"
+  },
+  {
+    "revision": "b08e3d07b5e0163e4b68",
+    "url": "/annaavanesyan/todo-react-node/static/js/runtime~main.b09a0f10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/annaavanesyan/todo-react-node/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/annaavanesyan/todo-react-node/precache-manifest.981d6dad41499e96cdac6561a5365d41.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/annaavanesyan/todo-react-node/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});

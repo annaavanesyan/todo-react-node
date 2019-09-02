14ff07e32756ccf0a6f7d6c9f88fc76203e51bb4
annaavanesyan <anna.avanesyan1998@gmail.com>
2019-09-02 19:44:01 +0400

    added server with db connection and routers

---
+++ server/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+const config = require('./config');
+
+const app = new express();
+
+/** DATABASE **/
+const db = require('./data');
+
+/** MIDDLEWARES **/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/** ROUTES **/
+app.use('/', routes);
+
+/** SERVER CONFIGURATION **/
+const server = require('http').createServer(app);
+const port = process.env.SERVER_PORT || config.get('SERVER_PORT');
+
+server.listen(port, () => { console.info(`Server is running on port: ${port}`) });
